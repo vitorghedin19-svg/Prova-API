38d9f2feef81351e91abd386b755267e771da1f0
VITOR CASAGRANDE GHEDIN <vitor.ghedin@alunos.sc.senac.br>
2026-06-11 19:10:37 -0300

    Início

---
+++ src/main/java/br/com/senac/Prova_API/ProvaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.Prova_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/Prova_API/ProvaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.Prova_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
